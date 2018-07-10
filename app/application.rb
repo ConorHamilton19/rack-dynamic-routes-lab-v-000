c9716f5f9b76de22c7998b5da3e29ccb0534de62
Conor Hamilton <conhamil19@gmail.com>
2018-07-10 19:18:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,15 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      
+      if @@items.include?(item)
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
       end
     resp.finish
   end
